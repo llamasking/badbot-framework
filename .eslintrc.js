ee3653e94bea7ac3b674734bc6ed75aed0c0415e
llamasking <21324788+llamasking@users.noreply.github.com>
2020-08-02 19:37:58 -0700

    Minor tweak to example command
    Also I just want codacy to rescan this.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true,
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    "parserOptions": {
-        "ecmaVersion": 2018
+    parserOptions: {
+        ecmaVersion: 2018,
     },
-    "rules": {
-        "curly": "off",
-        "single": "off",
-        "quotes": [1, "single", { "avoidEscape": true }],
-        "no-console": "off",
-        "semi": "warn",
-        "eqeqeq": "warn"
-    }
+    rules: {
+        curly: 'off',
+        single: 'off',
+        quotes: [1, 'single', { avoidEscape: true }],
+        'no-console': 'off',
+        semi: 'warn',
+        eqeqeq: 'warn',
+    },
 };
